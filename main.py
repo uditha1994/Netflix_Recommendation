a3bbf36b68e2dddd2d08408aa20fce9effc03fa1
uditha1994 <uditha@programmer.net>
2024-01-20 20:05:58 +0530

    dataset import

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 # import  os
 # print(os.getcwd())
 # print(os.listdir())
+from datetime import date
 
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction import text
 from sklearn.metrics.pairwise import cosine_similarity
+import nltk
+import re
 
 data = pd.read_csv("/home/udithal/Downloads/archive/netflixData.csv")
-print(data.head())
+# print(data.head(10))
+# print(data.isnull().sum())
+
+# slicing data
+data = data[["Title","Description","Content Type","Genres"]]
+# print(data.isnull().sum())
+# print(data.head())
+
+data = data.dropna()
+print(data)
+
+nltk.download('stopwords')
+stemmer = nltk.SnowballStemmer("english")
